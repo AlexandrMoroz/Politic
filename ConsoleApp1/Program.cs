58f536d5432242ecbeec279ef011556f2d7828eb
Алекс <@->
2018-12-14 19:25:28 +0200

    new

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,12 +7,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-  public class a {
-        public virtual string SS () => Console.WriteLine('a');
-  }
-    public class B : a {
-        public override string SS () =>  Console.WriteLine('b');
-    }
+ 
     class Program
     {
 
